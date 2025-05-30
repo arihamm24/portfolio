060f3db81a4aabd1a7b70964217b61451d3409c7
Ari Hammond <ajhammond06@gmail.com>
2025-05-30 14:18:22 -0400

    navbar structure
    in mobile mode: hamburger
    in desktop: normal list
    
    no JS functionality yet

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 import computer from '../../assets/projects/computer_2_cool-0.png'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import menu from '../../assets/navbar/menu.svg'
 
 const Navbar = () => {
+
     return (
         <div>
             <nav className="navbar">
                 <Link to="/" className="navHeader">
                     <img src={computer} alt="windows 95 computer icon" />
-                    <h1>Ari Hammond</h1>
+                    Ari Hammond
                 </Link>
-                <ul className="navMenu">
-                    <Link to="/" className="navItem">Home</Link>
-                    <Link to="/about" className="navItem">About</Link>
-                    <Link to="/projects" className="navItem">Projects</Link>
-                </ul>
+                <div className="navMenu">
+                    <img src={menu} alt="menu hamburger icon white" className="menuIcon" />
+                    <ul className="menuItems">
+                        <li><Link to="/" className="navItem">Home</Link></li>
+                        <li><Link to="about" className="navItem">About</Link></li>
+                        <li><Link to="/projects" className="navItem">Projects</Link></li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
