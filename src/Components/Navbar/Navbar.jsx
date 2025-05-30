98285a078772f74ffa352757973a66b57ed78bd6
Ari Hammond <ajhammond06@gmail.com>
2025-05-30 15:35:37 -0400

    navbar structure attempts

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import computer from '../../assets/projects/computer_2_cool-0.png'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import menu from '../../assets/navbar/menu.svg'
 
 const Navbar = () => {
-
+    function mobileNav() {
+        alert("Mobile Navbar not functional");
+    }
     return (
         <div>
             <nav className="navbar">
@@ -14,7 +16,7 @@ const Navbar = () => {
                     Ari Hammond
                 </Link>
                 <div className="navMenu">
-                    <img src={menu} alt="menu hamburger icon white" className="menuIcon" />
+                    <img src={menu} onClick={mobileNav} alt="menu hamburger icon white" className="menuIcon"/>
                     <ul className="menuItems">
                         <li><Link to="/" className="navItem">Home</Link></li>
                         <li><Link to="about" className="navItem">About</Link></li>
