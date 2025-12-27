ab24bf3aa9b448876b8f73fca02126609d7805d0
Ari Hammond <ajhammond06@gmail.com>
2025-12-26 19:29:47 -0500

    FINISHED CA 2025

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
 import CaseStudy from './components/caseStudies/caseStudy.jsx';
+import ScrollToTop from './components/scrollToTop.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
   <BrowserRouter>
+    <ScrollToTop/>
     <Routes>
       <Route index element={<App />}></Route>
       <Route path='/:id' element={<CaseStudy/>}/>
