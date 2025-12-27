700823508ed33fa1417c98146d4f48127d89b7fa
Ari Hammond <ajhammond06@gmail.com>
2025-12-27 18:18:10 -0500

    hosting cv/resume in drive

--- src/components/buttons/actionButtons.jsx
+++ src/components/buttons/actionButtons.jsx
@@ -1,8 +1,8 @@
 function ActionButtons() {
     return (
         <div id="actionButtons">
-            <button><a href="#" type="application.pdf" download>Academic CV</a></button>
-            <button><a href="././public/documents/Hammond_Ari_Resume.pdf" type="application.pdf" download>Resume</a></button>
+            <button><a href="#" type="application.pdf">Academic CV</a></button>
+            <button><a href="https://drive.google.com/file/d/1T2Uuc4yv5YrstYMTzhYK3j5mQb2fljH2/view?usp=drive_link" type="application.pdf">Resume</a></button>
         </div>
     );
 }
